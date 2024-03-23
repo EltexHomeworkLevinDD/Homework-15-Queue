77b30029cbec7f8a29162138874eb1c637b1f277
EltexHomeworkLevinDD <pykanus172@gmail.com>
2024-03-23 21:11:57 +0700

    task-1 выполнен

---
+++ src/submain.h
@@ -0,0 +1,35 @@
+#ifndef SUBMAIN_H
+#define SUBMAIN_H
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif//_GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/msg.h>
+#include "submain.h"
+
+#define QUEUE_FILE "src/main.c"
+#define QUEUE_PROJ_ID 256
+#define MTEXT_MAX_SIZE 20
+
+#define EXITMSG(msg) do { \
+        perror(msg); \
+        exit(EXIT_FAILURE); } while(0)
+
+struct mymsgbuf{
+    long mtype;
+    char mtext[MTEXT_MAX_SIZE];
+};
+
+void get_msg(int qid, int msgtype, const char* prefix);
+void send_msg(int qid, int msgtype, const char* prefix, const char* text);
+char* get_full_path(const char* path);
+
+#endif//SUBMAIN_H
